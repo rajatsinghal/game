c9080d7d5d39cb026a593c30e39a356e5613f554
Rajat Singhal <rajat.developer.singhal@gmail.com>
2012-12-02 21:49:46 +0530

    sum shown

--- dice.js
+++ dice.js
@@ -3,6 +3,7 @@ function animateRoll(times) {
   var roll = generateRoll();
   drawRoll(roll[0], roll[1]);
   if (times == 11) {
+    showResult(roll[0], roll[1]);
     return;
   }
   setTimeout('animateRoll(' + (times + 1) + ')', 200);
@@ -12,7 +13,11 @@ function generateRoll() {
   return [ Math.floor(Math.random()*6) + 1, Math.floor(Math.random()*6) + 1 ];
 }
 
-function drawRoll(die1, die2) {
-  $('#die1').html('<img src="images/dice_' + die1 +'.gif" />');
-  $('#die2').html('<img src="images/dice_' + die2 +'.gif" />');
+function drawRoll(dice1, dice2) {
+  $('#die1').html('<img src="images/dice_' + dice1 +'.gif" />');
+  $('#die2').html('<img src="images/dice_' + dice2 +'.gif" />');
+}
+
+function showResult(dice1, dice2) {
+  $('#sum').html('Total: '+(dice1 + dice2));
 }
